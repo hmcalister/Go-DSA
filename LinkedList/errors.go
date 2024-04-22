8c42c37fad9198c8bcc226102e95e5e51565e086
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-22 23:48:16 +1200

    Add errors for linkedlist package

---
+++ LinkedList/errors.go
@@ -0,0 +1,19 @@
+package linkedlist
+
+import "fmt"
+
+type IndexOutOfBoundsError struct {
+	targetIndex int
+	listLength  int
+}
+
+func (e *IndexOutOfBoundsError) Error() string {
+	return fmt.Sprintf("index %d out of bound for list of length %d", e.targetIndex, e.listLength)
+}
+
+type EmptyListError struct {
+}
+
+func (e *EmptyListError) Error() string {
+	return fmt.Sprint("cannot perform operation on an empty list")
+}
