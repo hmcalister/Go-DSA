dd23f084aeaa946758b6d6275dc01788d3e801e4
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-22 23:46:02 +1200

    Add documentation to LinkedList fields

--- LinkedList/linkedList.go
+++ LinkedList/linkedList.go
@@ -1,7 +1,17 @@
 package linkedlist
 
 type LinkedList[T any] struct {
+	// Head of the list, the first Node
+	//
+	// nil only when the length is zero
 	head *LinkedListNode[T]
+	// Tail of the list, the last Node
+	//
+	// nil only when the length is zero
+	tail *LinkedListNode[T]
+
+	// Length of the list, the total number of Nodes
+	length int
 }
 
 type LinkedListNode[T any] struct {
