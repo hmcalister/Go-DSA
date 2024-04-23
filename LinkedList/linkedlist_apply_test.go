9be4d257f6d57a755867e04e3fa6c4b4b0819cf4
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-23 22:14:02 +1200

    Add test for ForwardFold

--- LinkedList/linkedlist_apply_test.go
+++ LinkedList/linkedlist_apply_test.go
@@ -94,3 +94,23 @@ func TestReverseMap(t *testing.T) {
 	}
 }
 
+func TestForwardFold(t *testing.T) {
+	list := linkedlist.New[string]()
+	items := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	for _, item := range items {
+		list.Add(item)
+	}
+
+	concatString := linkedlist.ForwardFold(list, "", func(item string, accumulator string) string {
+		return accumulator + item
+	})
+	expectedConcatString := ""
+	for _, item := range items {
+		expectedConcatString += item
+	}
+
+	if concatString != expectedConcatString {
+		t.Errorf("result (%v) does not match expected result (%v)", concatString, expectedConcatString)
+	}
+}
+
