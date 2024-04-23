ff97a1dfcc45d6a4dde209c008e05447f051cab3
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-23 23:26:21 +1200

    Add default float comparators

--- DefaultComparators/defaultComparators.go
+++ DefaultComparators/defaultComparators.go
@@ -7,3 +7,29 @@ func DefaultIntegerComparator(a, b int) int {
 	return a - b
 }
 
+// Floats are difficult to compare, and hence we need some slightly ugly logic
+
+func DefaultFloat32Comparator(a, b float32) int {
+	if a < b {
+		return -1
+	}
+
+	if a > b {
+		return +1
+	}
+
+	return 0
+}
+
+func DefaultFloat64Comparator(a, b float64) int {
+	if a < b {
+		return -1
+	}
+
+	if a > b {
+		return +1
+	}
+
+	return 0
+}
+
