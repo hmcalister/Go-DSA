ad0dfe025585465a3061c4d982442ab9af7379bc
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-23 23:26:08 +1200

    Add DefaultIntegerComparator

--- DefaultComparators/defaultComparators.go
+++ DefaultComparators/defaultComparators.go
@@ -3,3 +3,7 @@ package defaultcomparators
 // Comparators take two items, a and b, and returns a negative value if a<b, a positive value if a>b, and zero if a==b
 type ComparatorFunction[T any] func(a, b T) int
 
+func DefaultIntegerComparator(a, b int) int {
+	return a - b
+}
+
