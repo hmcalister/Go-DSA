0b2bf1a18a897e27493c675588a3586159b5cca0
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-23 22:09:32 +1200

    Add error separator comments

--- LinkedList/errors.go
+++ LinkedList/errors.go
@@ -2,6 +2,7 @@ package linkedlist
 
 import "fmt"
 
+// ----------------------------------------------------------------------------
 type IndexOutOfBoundsError struct {
 	targetIndex int
 	listLength  int
@@ -11,9 +12,12 @@ func (e *IndexOutOfBoundsError) Error() string {
 	return fmt.Sprintf("index %d out of bound for list of length %d", e.targetIndex, e.listLength)
 }
 
+// ----------------------------------------------------------------------------
 type EmptyListError struct {
 }
 
 func (e *EmptyListError) Error() string {
 	return "cannot perform operation on an empty list"
 }
+
+// ----------------------------------------------------------------------------
