8808e3ea43d25ed29f663a956dc020f2cfe66d96
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-24 01:36:05 +1200

    Add an error for when an item is not present in the tree

--- BinarySearchTree/errors.go
+++ BinarySearchTree/errors.go
@@ -12,3 +12,12 @@ func (e *ItemAlreadyPresentError[T]) Error() string {
 	return fmt.Sprintf("item %#v already present in binary tree", e.item)
 }
 
+// Error for if item does not exists in the tree
+
+type ItemNotFoundError[T any] struct {
+	item T
+}
+
+func (e *ItemNotFoundError[T]) Error() string {
+	return fmt.Sprintf("item %#v not present in binary tree", e.item)
+}
