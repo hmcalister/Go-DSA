8e744e8061c9687ef05083e7c1eba0bffaccd43a
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-24 01:49:10 +1200

    Add function to create a new BST

--- BinarySearchTree/binaryserachtree.go
+++ BinarySearchTree/binaryserachtree.go
@@ -12,4 +12,14 @@ type BinarySearchTree[T any] struct {
 	comparatorFunction comparator.ComparatorFunction[T]
 }
 
+// Create a new binary tree of generic type.
+//
+// The comparator function (see github.com/hmcalister/Go-DSA/Comparator) defines how the items are ordered when creating the tree.
+// This allows for trees that have any type, rather than just comparable types.
+func New[T any](comparatorFunction comparator.ComparatorFunction[T]) *BinarySearchTree[T] {
+	return &BinarySearchTree[T]{
+		root:               nil,
+		comparatorFunction: comparatorFunction,
+	}
 }
+
