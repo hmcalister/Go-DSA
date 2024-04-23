0c7676df5b687ab022754e99d44d9f12b0230134
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-23 22:13:24 +1200

    Add test for ForwardApply

--- LinkedList/linkedlist_apply_test.go
+++ LinkedList/linkedlist_apply_test.go
@@ -8,3 +8,22 @@ import (
 	linkedlist "github.com/hmcalister/Go-DSA/LinkedList"
 )
 
+func TestForwardApply(t *testing.T) {
+	list := linkedlist.New[string]()
+	items := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	for _, item := range items {
+		list.Add(item)
+	}
+
+	concatString := ""
+	list.ForwardApply(func(item string) { concatString += item })
+	expectedConcatString := ""
+	for _, item := range items {
+		expectedConcatString += item
+	}
+
+	if concatString != expectedConcatString {
+		t.Errorf("result (%v) does not match expected result (%v)", concatString, expectedConcatString)
+	}
+}
+
