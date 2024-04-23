a9133364208d5fd15b1725695f81f301f4a2bfc8
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-23 16:43:07 +1200

    Add test to check item at index after simple Add

--- LinkedList/linkedList_get_test.go
+++ LinkedList/linkedList_get_test.go
@@ -25,3 +25,51 @@ func TestItemAtIndex(t *testing.T) {
 	}
 }
 
+// Tests adding items to a list at an index, then getting from that index.
+func TestGetAtIndexAfterAdd(t *testing.T) {
+	items := []int{10, 20, 30, 40, 50, 60, 70, 80, 90}
+	newItem := 0
+
+	// Define a small helper method that creates a new list,
+	// inserts the items from the items array, then tries to insert an item
+	// at the specified index.
+	//
+	// Calls t.Errorf if the insert fails or if the list length does not match the expected
+	addHelper := func(t *testing.T, addIndex int) {
+		list := linkedlist.New[int]()
+		for _, item := range items {
+			list.Add(item)
+		}
+
+		err := list.AddAtIndex(newItem, addIndex)
+		if err != nil {
+			t.Errorf("error when adding item to list: %v", err)
+		}
+
+		retrievedItem, err := list.ItemAtIndex(addIndex)
+
+		if retrievedItem != newItem {
+			t.Errorf("retrieved item (%v) does not match inserted item (%v)", retrievedItem, newItem)
+		}
+		if err != nil {
+			t.Errorf("error when getting item at new index: %v", err)
+		}
+	}
+
+	t.Run("get at head index", func(t *testing.T) {
+		addHelper(t, 0)
+	})
+
+	t.Run("get at non-head first-half index", func(t *testing.T) {
+		addHelper(t, 2)
+	})
+
+	t.Run("get at non-head second-half index", func(t *testing.T) {
+		addHelper(t, len(items)-3)
+	})
+
+	t.Run("get at tail index", func(t *testing.T) {
+		addHelper(t, len(items)-1)
+	})
+}
+
