47a20354706f236f3fa03eb4179fb6058633c157
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-24 23:18:41 +1200

    Add RedBlackTreeNode struct

--- RedBlackTree/node.go
+++ RedBlackTree/node.go
@@ -6,3 +6,27 @@ const (
 	BLACK colorEnum = iota
 	RED   colorEnum = iota
 )
+
+type RedBlackTreeNode[T any] struct {
+	// The item of this node
+	item T
+
+	// The size of this node, the number of nodes in this subtree
+	// (count of this node and all children)
+	size int
+
+	// The height of this node, the number of steps to the furthest leaf node
+	height int
+
+	// The color of this node, either RED or BLACK
+	color colorEnum
+
+	// The parent of this node
+	parent *RedBlackTreeNode[T]
+
+	// The left child of this node
+	left *RedBlackTreeNode[T]
+
+	// The right child of this node
+	right *RedBlackTreeNode[T]
+}
