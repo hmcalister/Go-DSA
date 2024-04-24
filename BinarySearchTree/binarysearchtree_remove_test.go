0b0a9a9e59e715fe52811a12906914f7612e48b4
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-24 23:06:55 +1200

    Add helper method to test height after removal

--- BinarySearchTree/binarysearchtree_remove_test.go
+++ BinarySearchTree/binarysearchtree_remove_test.go
@@ -328,3 +328,31 @@ func TestSizeAfterRemoval(t *testing.T) {
 	})
 }
 
+// ----------------------------------------------------------------------------
+// Height Tests
+
+func TestHeightAfterRemoval(t *testing.T) {
+	// Define a helper function that creates a new tree, removes an item, and tests each remaining item against the expected size map
+	testHeightAfterRemovalHelper := func(t *testing.T, items []int, removalItem int, expectedHeightMap map[int]int) {
+		tree := binarysearchtree.New[int](comparator.DefaultIntegerComparator)
+		for _, item := range items {
+			tree.Add(item)
+		}
+
+		err := tree.Remove(removalItem)
+		if err != nil {
+			t.Errorf("error (%v) encountered when removing item", err)
+		}
+
+		for item, expectedHeight := range expectedHeightMap {
+			node, err := tree.Find(item)
+			if err != nil {
+				t.Errorf("error (%v) encountered when finding item that was inserted into tree", err)
+			}
+
+			if node.Height() != expectedHeight {
+				t.Errorf("found height (%v) does not match expected height (%v) for item %v", node.Size(), expectedHeight, item)
+			}
+		}
+	}
+
