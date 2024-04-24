c8815e7447ab4a504e840a43993ca09ba07ead69
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-24 23:27:35 +1200

    Add a method to create a new red-black tree

--- RedBlackTree/redblacktree.go
+++ RedBlackTree/redblacktree.go
@@ -9,3 +9,15 @@ type RedBlackTree[T any] struct {
 	// Comparator function to compare and order the type T
 	comparatorFunction comparator.ComparatorFunction[T]
 }
+
+// Create a new red-black tree of generic type.
+//
+// The comparator function (see github.com/hmcalister/Go-DSA/Comparator) defines how the items are ordered when creating the tree.
+// This allows for trees that have any type, rather than just comparable types.
+func New[T any](comparatorFunction comparator.ComparatorFunction[T]) *RedBlackTree[T] {
+	return &RedBlackTree[T]{
+		root:               nil,
+		comparatorFunction: comparatorFunction,
+	}
+}
+
