38ab2512fcd7fe77fd1170c65822b9fa7ef980f8
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-26 00:06:12 +1200

    Add test for initialization of various types of trees

---
+++ RedBlackTree/redblacktree_misc_test.go
@@ -0,0 +1,38 @@
+package redblacktree_test
+
+import (
+	"testing"
+
+	comparator "github.com/hmcalister/Go-DSA/Comparator"
+	redblacktree "github.com/hmcalister/Go-DSA/RedBlackTree"
+)
+
+func TestInitializeTreeGenericTypes(t *testing.T) {
+	t.Run("rbt int", func(t *testing.T) {
+		redblacktree.New[int](comparator.DefaultIntegerComparator)
+	})
+
+	t.Run("rbt float", func(t *testing.T) {
+		redblacktree.New[float64](comparator.DefaultFloat64Comparator)
+	})
+
+	t.Run("rbt string", func(t *testing.T) {
+		redblacktree.New[string](comparator.DefaultStringComparator)
+	})
+
+	type S struct {
+		i int
+		_ float64
+		_ string
+	}
+	t.Run("rbt struct", func(t *testing.T) {
+		redblacktree.New[S](func(a, b S) int {
+			if a.i < b.i {
+				return -1
+			} else if a.i > b.i {
+				return 1
+			}
+			return 0
+		})
+	})
+}
