3d48ff54a126ca34b97d2253e096f96da93ec858
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-27 02:32:08 +1200

    Implement a New method for min heaps

--- Heap/minBinaryHeap.go
+++ Heap/minBinaryHeap.go
@@ -9,3 +9,21 @@ type MinBinaryHeap[T any] struct {
 	comparatorFunction comparator.ComparatorFunction[T]
 }
 
+// Create a new Min-BinaryHeap, with comparator given by the comparatorFunction.
+//
+// See `github.com/hmcalister/Go-DSA/Comparator` for more information on the comparator.
+func NewMinBinaryHeap[T any](comparatorFunction comparator.ComparatorFunction[T]) *MinBinaryHeap[T] {
+	return &MinBinaryHeap[T]{
+		// Store the heap as an array.
+		// The root is stored in heapData[0], then recursively the
+		// node at index `i` has left child at `2i+1` and right child at `2i+2`.
+		// Therefore, the parent of a node is given by floor( (i-1) / 2 ).
+		//
+		// Note that slices are backed by arrays, which is doubled in size when needed
+		// making the append calls very cheap (amortized).
+		// See https://go.dev/blog/slices
+		heapData:           make([]T, 0),
+		comparatorFunction: comparatorFunction,
+	}
+}
+
