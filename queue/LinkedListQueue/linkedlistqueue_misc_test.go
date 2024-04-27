efb1407a48a3557ddff6b6c0fd28025720ab237e
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 00:13:38 +1200

    Add test to initalize various linked list queues of different types

---
+++ queue/LinkedListQueue/linkedlistqueue_misc_test.go
@@ -0,0 +1,28 @@
+package linkedlistqueue_test
+
+import (
+	"testing"
+
+	linkedlistqueue "github.com/hmcalister/Go-DSA/queue/LinkedListQueue"
+)
+
+func TestLinkedListQueueInit(t *testing.T) {
+	t.Run("linked list int", func(t *testing.T) {
+		linkedlistqueue.New[int]()
+	})
+	t.Run("linked list float", func(t *testing.T) {
+		linkedlistqueue.New[float64]()
+	})
+	t.Run("linked list string", func(t *testing.T) {
+		linkedlistqueue.New[string]()
+	})
+	t.Run("linked list struct", func(t *testing.T) {
+		type S struct {
+			_ int
+			_ float64
+			_ string
+		}
+		linkedlistqueue.New[S]()
+	})
+}
+
