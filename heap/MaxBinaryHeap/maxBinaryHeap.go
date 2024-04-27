c75d2e2e25c6a00c9133311a8f86789fd8b76201
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-27 23:15:56 +1200

    Update max heap with succinct New function

--- heap/MaxBinaryHeap/maxBinaryHeap.go
+++ heap/MaxBinaryHeap/maxBinaryHeap.go
@@ -12,7 +12,7 @@ type MaxBinaryHeap[T any] struct {
 // Create a new max-BinaryHeap, with comparator given by the comparatorFunction.
 //
 // See `github.com/hmcalister/Go-DSA/Comparator` for more information on the comparator.
-func NewMaxBinaryHeap[T any](comparatorFunction comparator.ComparatorFunction[T]) *MaxBinaryHeap[T] {
+func New[T any](comparatorFunction comparator.ComparatorFunction[T]) *MaxBinaryHeap[T] {
 	return &MaxBinaryHeap[T]{
 		// Store the heap as an array.
 		// The root is stored in heapData[0], then recursively the
