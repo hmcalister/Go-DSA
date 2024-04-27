6e1da9ce310d780b2753753886a6f7264361e2f2
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 00:14:28 +1200

    Add test for peeking after removal from linked list queue

--- queue/LinkedListQueue/linkedlistqueue_remove_test.go
+++ queue/LinkedListQueue/linkedlistqueue_remove_test.go
@@ -31,3 +31,29 @@ func TestLinkedListQueueRemoveFromEmptyQueue(t *testing.T) {
 	}
 }
 
+func TestLinkedListQueueCheckPeekAfterRemove(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	queue := linkedlistqueue.New[int]()
+
+	for _, item := range items {
+		queue.Add(item)
+	}
+
+	for _, item := range items {
+		peekItem, err := queue.Peek()
+		if err != nil {
+			t.Errorf("encountered error (%v) after peeking at non-empty queue", err)
+		}
+
+		expectedItem := item
+		if peekItem != expectedItem {
+			t.Errorf("found peek item (%v) does not match the expected item (%v)", peekItem, expectedItem)
+		}
+
+		_, err = queue.Remove()
+		if err != nil {
+			t.Errorf("encountered error (%v) when removing from non-empty queue", err)
+		}
+	}
+}
+
