d9e7ed1c5f76371aee1ea3ab9a9a970e329f94ff
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 01:17:07 +1200

    Add test for checking the peek item after removing from a priority queue

--- queue/PriorityQueue/linkedlistqueue_remove_test.go
+++ queue/PriorityQueue/linkedlistqueue_remove_test.go
@@ -33,3 +33,29 @@ func TestPriorityQueueRemoveFromEmptyQueue(t *testing.T) {
 	}
 }
 
+func TestPriorityQueueCheckPeekAfterRemove(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	queue := priorityqueue.New[int](comparator.DefaultIntegerComparator)
+
+	for _, item := range items {
+		queue.Add(item)
+	}
+
+	for _, item := range items {
+		peekItem, err := queue.Peek()
+		if err != nil {
+			t.Errorf("encountered error (%v) after peeking at non-empty queue", err)
+		}
+
+		expectedItem := item
+		if peekItem != expectedItem {
+			t.Errorf("found peek item (%v) does not match the expected item (%v)", peekItem, expectedItem)
+		}
+
+		_, err = queue.Remove()
+		if err != nil {
+			t.Errorf("encountered error (%v) when removing from non-empty queue", err)
+		}
+	}
+}
+
