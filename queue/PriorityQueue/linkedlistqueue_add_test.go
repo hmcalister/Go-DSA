6f2ca41fbdb6774cf994214469022c22e117b97d
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 01:15:56 +1200

    Test peeking after addition to a priority queue

--- queue/PriorityQueue/linkedlistqueue_add_test.go
+++ queue/PriorityQueue/linkedlistqueue_add_test.go
@@ -16,3 +16,22 @@ func TestPriorityQueueAdd(t *testing.T) {
 	}
 }
 
+func TestPriorityQueueCheckPeekAfterAddInPriorityOrder(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	queue := priorityqueue.New[int](comparator.DefaultIntegerComparator)
+
+	for _, item := range items {
+		queue.Add(item)
+
+		peekItem, err := queue.Peek()
+		if err != nil {
+			t.Errorf("encountered error (%v) after peeking at non-empty queue", err)
+		}
+
+		expectedItem := items[0]
+		if peekItem != expectedItem {
+			t.Errorf("found peek item (%v) does not match the expected item (%v)", peekItem, expectedItem)
+		}
+	}
+}
+
