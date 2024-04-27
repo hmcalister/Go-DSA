66f740072fd902fad1e62d2b35e08b0560412e3c
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-27 23:57:32 +1200

    Add New method for LinkedListQueue

--- queue/LinkedListQueue/linkedlistqueue.go
+++ queue/LinkedListQueue/linkedlistqueue.go
@@ -7,3 +7,10 @@ type LinkedListQueue[T any] struct {
 	queueData *linkedlist.LinkedList[T]
 }
 
+// Create a new LinkedListQueue using github.com/hmcalister/Go-DSA/list/LinkedList as a backing data structure.
+func New[T any]() *LinkedListQueue[T] {
+	return &LinkedListQueue[T]{
+		queueData: linkedlist.New[T](),
+	}
+}
+
