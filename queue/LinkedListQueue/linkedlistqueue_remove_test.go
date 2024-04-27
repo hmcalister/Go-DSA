3c4179dde97f19891d5bd0e3150c03e21610669a
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 00:13:58 +1200

    Add test for removing from a linked list queue

---
+++ queue/LinkedListQueue/linkedlistqueue_remove_test.go
@@ -0,0 +1,24 @@
+package linkedlistqueue_test
+
+import (
+	"testing"
+
+	linkedlistqueue "github.com/hmcalister/Go-DSA/queue/LinkedListQueue"
+)
+
+func TestLinkedListQueueRemove(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	queue := linkedlistqueue.New[int]()
+
+	for _, item := range items {
+		queue.Add(item)
+	}
+
+	for range items {
+		_, err := queue.Remove()
+		if err != nil {
+			t.Errorf("encountered error (%v) when removing from non-empty queue", err)
+		}
+	}
+}
+
