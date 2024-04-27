17abe07bba19332507657cffdb46c81cb1017ae7
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 01:16:40 +1200

    Add test for remove method in priority queue

---
+++ queue/PriorityQueue/linkedlistqueue_remove_test.go
@@ -0,0 +1,26 @@
+package priorityqueue_test
+
+import (
+	"slices"
+	"testing"
+
+	comparator "github.com/hmcalister/Go-DSA/Comparator"
+	priorityqueue "github.com/hmcalister/Go-DSA/queue/PriorityQueue"
+)
+
+func TestPriorityQueueRemove(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	queue := priorityqueue.New[int](comparator.DefaultIntegerComparator)
+
+	for _, item := range items {
+		queue.Add(item)
+	}
+
+	for range items {
+		_, err := queue.Remove()
+		if err != nil {
+			t.Errorf("encountered error (%v) when removing from non-empty queue", err)
+		}
+	}
+}
+
