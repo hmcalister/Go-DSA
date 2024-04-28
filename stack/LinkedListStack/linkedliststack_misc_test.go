9fc997604597dc2d050e8b2b1353a099d9a72b42
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 16:09:45 +1200

    Add test for initalizing stacks of various data types

---
+++ stack/LinkedListStack/linkedliststack_misc_test.go
@@ -0,0 +1,28 @@
+package linkedliststack_test
+
+import (
+	"testing"
+
+	linkedliststack "github.com/hmcalister/Go-DSA/stack/LinkedListStack"
+)
+
+func TestLinkedListStackInit(t *testing.T) {
+	t.Run("linked list int", func(t *testing.T) {
+		linkedliststack.New[int]()
+	})
+	t.Run("linked list float", func(t *testing.T) {
+		linkedliststack.New[float64]()
+	})
+	t.Run("linked list string", func(t *testing.T) {
+		linkedliststack.New[string]()
+	})
+	t.Run("linked list struct", func(t *testing.T) {
+		type S struct {
+			_ int
+			_ float64
+			_ string
+		}
+		linkedliststack.New[S]()
+	})
+}
+
