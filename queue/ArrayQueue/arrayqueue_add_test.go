dbeecbba79e3e6afd888c587d781ca90f912f558
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 16:36:08 +1200

    Add test to check queue size after addition to array queue

--- queue/ArrayQueue/arrayqueue_add_test.go
+++ queue/ArrayQueue/arrayqueue_add_test.go
@@ -34,3 +34,17 @@ func TestArrayQueueCheckPeekAfterAdd(t *testing.T) {
 	}
 }
 
+func TestArrayQueueCheckSizeAfterAdd(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	queue := arrayqueue.New[int]()
+
+	for index, item := range items {
+		queue.Add(item)
+
+		queueSize := queue.Size()
+		expectedSize := index + 1
+		if queueSize != expectedSize {
+			t.Errorf("found queue size (%v) does not match the expected size (%v)", queueSize, expectedSize)
+		}
+	}
+}
