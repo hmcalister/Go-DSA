e6c3ac83070b6ea197b1da0766d968be7aa200c9
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 16:08:25 +1200

    Add a New function to make a new LinkedListStack

--- stack/LinkedListStack/linkedliststack.go
+++ stack/LinkedListStack/linkedliststack.go
@@ -9,3 +9,10 @@ type LinkedListStack[T any] struct {
 	stackData *linkedlist.LinkedList[T]
 }
 
+// Create a new LinkedListStack using github.com/hmcalister/Go-DSA/list/LinkedList as a backing data structure.
+func New[T any]() *LinkedListStack[T] {
+	return &LinkedListStack[T]{
+		stackData: linkedlist.New[T](),
+	}
+}
+
