4eee75abd4ff17d2bd2410a89223b7e58693fa5e
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 16:34:40 +1200

    Add method to create a new arrayqueue

--- queue/ArrayQueue/arrayqueue.go
+++ queue/ArrayQueue/arrayqueue.go
@@ -7,3 +7,13 @@ type ArrayQueue[T any] struct {
 	queueData []T
 }
 
+// Create a new ArrayQueue using github.com/hmcalister/Go-DSA/list/ArrayQueue as a backing data structure.
+func New[T any]() *ArrayQueue[T] {
+	return &ArrayQueue[T]{
+		// Slices are backed by arrays which grow with a growth factor of 2.
+		//
+		// This will be fine for our purposes.
+		queueData: make([]T, 0),
+	}
+}
+
