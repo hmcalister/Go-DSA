555b4f23c00a0b408825cb80f5f4290219acf9ef
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 16:24:34 +1200

    Add test for peeking at stack after add

--- stack/ArrayStack/arraystack_add_test.go
+++ stack/ArrayStack/arraystack_add_test.go
@@ -15,3 +15,22 @@ func TestArrayStackAdd(t *testing.T) {
 	}
 }
 
+func TestArrayStackCheckPeekAfterAdd(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	stack := arraystack.New[int]()
+
+	for _, item := range items {
+		stack.Add(item)
+
+		peekItem, err := stack.Peek()
+		if err != nil {
+			t.Errorf("encountered error (%v) after peeking at non-empty stack", err)
+		}
+
+		expectedItem := item
+		if peekItem != expectedItem {
+			t.Errorf("found peek item (%v) does not match the expected item (%v)", peekItem, expectedItem)
+		}
+	}
+}
+
