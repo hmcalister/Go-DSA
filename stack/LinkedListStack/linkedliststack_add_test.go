be0468b4eb91ef98195c767622a33294511a7656
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 16:10:12 +1200

    Add test for adding to stack

---
+++ stack/LinkedListStack/linkedliststack_add_test.go
@@ -0,0 +1,17 @@
+package linkedliststack_test
+
+import (
+	"testing"
+
+	linkedliststack "github.com/hmcalister/Go-DSA/stack/LinkedListStack"
+)
+
+func TestLinkedListStackAdd(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	stack := linkedliststack.New[int]()
+
+	for _, item := range items {
+		stack.Add(item)
+	}
+}
+
