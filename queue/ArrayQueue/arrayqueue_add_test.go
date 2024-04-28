cb67b4c3cab7c94f3dc08d0b2a9256d1356e951b
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 16:35:49 +1200

    Add method to test peek after addition to arrayqueue

--- queue/ArrayQueue/arrayqueue_add_test.go
+++ queue/ArrayQueue/arrayqueue_add_test.go
@@ -15,3 +15,22 @@ func TestArrayQueueAdd(t *testing.T) {
 	}
 }
 
+func TestArrayQueueCheckPeekAfterAdd(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	queue := arrayqueue.New[int]()
+
+	for _, item := range items {
+		queue.Add(item)
+
+		peekItem, err := queue.Peek()
+		if err != nil {
+			t.Errorf("encountered error (%v) after peeking at non-empty queue", err)
+		}
+
+		expectedItem := items[0]
+		if peekItem != expectedItem {
+			t.Errorf("found peek item (%v) does not match the expected item (%v)", peekItem, expectedItem)
+		}
+	}
+}
+
