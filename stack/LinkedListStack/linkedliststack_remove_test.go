a9e7c42cf2b190cbe1d63e48af6b884af041cdac
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 16:10:48 +1200

    Add test for Remove method of linked list stack

---
+++ stack/LinkedListStack/linkedliststack_remove_test.go
@@ -0,0 +1,25 @@
+package linkedliststack_test
+
+import (
+	"slices"
+	"testing"
+
+	linkedliststack "github.com/hmcalister/Go-DSA/stack/LinkedListStack"
+)
+
+func TestLinkedListStackRemove(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	stack := linkedliststack.New[int]()
+
+	for _, item := range items {
+		stack.Add(item)
+	}
+
+	for range items {
+		_, err := stack.Remove()
+		if err != nil {
+			t.Errorf("encountered error (%v) when removing from non-empty stack", err)
+		}
+	}
+}
+
