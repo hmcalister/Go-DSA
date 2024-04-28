e3cdfb558ae46b6e944c0e80a7fd985080178905
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 16:36:42 +1200

    Add test to initalize arrayqueue of various data types

---
+++ queue/ArrayQueue/arrayqueue_misc_test.go
@@ -0,0 +1,28 @@
+package arrayqueue_test
+
+import (
+	"testing"
+
+	arrayqueue "github.com/hmcalister/Go-DSA/queue/ArrayQueue"
+)
+
+func TestArrayQueueInit(t *testing.T) {
+	t.Run("linked list int", func(t *testing.T) {
+		arrayqueue.New[int]()
+	})
+	t.Run("linked list float", func(t *testing.T) {
+		arrayqueue.New[float64]()
+	})
+	t.Run("linked list string", func(t *testing.T) {
+		arrayqueue.New[string]()
+	})
+	t.Run("linked list struct", func(t *testing.T) {
+		type S struct {
+			_ int
+			_ float64
+			_ string
+		}
+		arrayqueue.New[S]()
+	})
+}
+
