a4096371c8d1c10eefd61ce22a90e98bb0d05168
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 17:18:28 +1200

    Add test for FindAll from empty linkedlistqueue

--- queue/LinkedListQueue/linkedlistqueue_misc_test.go
+++ queue/LinkedListQueue/linkedlistqueue_misc_test.go
@@ -43,3 +43,12 @@ func TestFindFromEmptyLinkedListQueue(t *testing.T) {
 		t.Errorf("found nil error after finding from empty queue")
 	}
 }
+
+func TestFindAllFromEmptyLinkedListQueue(t *testing.T) {
+	queue := linkedlistqueue.New[int]()
+
+	items := queue.FindAll(func(item int) bool { return item == 1 })
+	if len(items) != 0 {
+		t.Errorf("found a non-zero number of items from an empty queue")
+	}
+}
