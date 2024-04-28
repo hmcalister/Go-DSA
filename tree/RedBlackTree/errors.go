251df01754ee3ed453b6d484e027a4988cdf8f8d
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 19:06:39 +1200

    Remove red black tree error file

--- tree/RedBlackTree/errors.go
+++ tree/RedBlackTree/errors.go
@@ -5,7 +5,5 @@ import (
 )
 
 var (
-	ErrorItemAlreadyPresent  = errors.New("item already present in binary tree")
-	ErrorItemNotFound        = errors.New("item not present in binary tree")
 	ErrorRotationNotPossible = errors.New("rotation is not possible around node")
 )
