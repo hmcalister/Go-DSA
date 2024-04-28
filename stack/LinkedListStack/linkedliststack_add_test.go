02e880fad25dd5a6d4c27d3c295bf44638c10283
Hayden McAlister <haydenmcalister49@gmail.com>
2024-04-28 16:10:21 +1200

    Add test for peeking after add to stack

--- stack/LinkedListStack/linkedliststack_add_test.go
+++ stack/LinkedListStack/linkedliststack_add_test.go
@@ -15,3 +15,21 @@ func TestLinkedListStackAdd(t *testing.T) {
 	}
 }
 
+func TestLinkedListStackCheckPeekAfterAdd(t *testing.T) {
+	items := []int{1, 2, 3, 4, 5}
+	stack := linkedliststack.New[int]()
+
+	for _, item := range items {
+		stack.Add(item)
+
+		peekItem, err := stack.Peek()
+		if err != nil {
+			t.Errorf("encountered error (%v) after peeking at non-empty stack", err)
+		}
+
+		expectedItem := item
+		if peekItem != expectedItem {
+			t.Errorf("found peek item (%v) does not match the expected item (%v)", peekItem, expectedItem)
+		}
+	}
+}
