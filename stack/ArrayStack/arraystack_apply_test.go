0218800cf8fe41e6ac1774dbc8f91cd81bc8c689
Hayden McAlister <haydenmcalister49@gmail.com>
2024-12-13 02:35:07 +1300

    Add apply, map, and fold method tests to array stack

---
+++ stack/ArrayStack/arraystack_apply_test.go
@@ -0,0 +1,122 @@
+package arraystack_test
+
+import (
+	"fmt"
+	"slices"
+	"testing"
+
+	arraystack "github.com/hmcalister/Go-DSA/stack/ArrayStack"
+)
+
+func TestForwardApply(t *testing.T) {
+	queue := arraystack.New[string]()
+	items := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	for _, item := range items {
+		queue.Add(item)
+	}
+
+	concatString := ""
+	arraystack.ForwardApply(queue, func(item string) { concatString += item })
+	expectedConcatString := ""
+	for _, item := range items {
+		expectedConcatString += item
+	}
+
+	if concatString != expectedConcatString {
+		t.Errorf("result (%v) does not match expected result (%v)", concatString, expectedConcatString)
+	}
+}
+
+func TestReverseApply(t *testing.T) {
+	queue := arraystack.New[string]()
+	items := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	for _, item := range items {
+		queue.Add(item)
+	}
+
+	concatString := ""
+	arraystack.ReverseApply(queue, func(item string) { concatString += item })
+
+	slices.Reverse(items)
+	expectedConcatString := ""
+	for _, item := range items {
+		expectedConcatString += item
+	}
+
+	if concatString != expectedConcatString {
+		t.Errorf("result (%v) does not match expected result (%v)", concatString, expectedConcatString)
+	}
+}
+
+func TestForwardMap(t *testing.T) {
+	queue := arraystack.New[string]()
+	items := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	for _, item := range items {
+		queue.Add(item)
+	}
+
+	globalCounter := 0
+	arraystack.ForwardMap(queue, func(item string) string {
+		newItem := fmt.Sprintf("%v, %v", item, globalCounter)
+		globalCounter += 1
+		return newItem
+	})
+}
+
+func TestReverseMap(t *testing.T) {
+	queue := arraystack.New[string]()
+	items := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	for _, item := range items {
+		queue.Add(item)
+	}
+
+	globalCounter := 0
+	arraystack.ReverseMap(queue, func(item string) string {
+		newItem := fmt.Sprintf("%v, %v", item, globalCounter)
+		globalCounter += 1
+		return newItem
+	})
+
+}
+
+func TestForwardFold(t *testing.T) {
+	queue := arraystack.New[string]()
+	items := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	for _, item := range items {
+		queue.Add(item)
+	}
+
+	concatString := arraystack.ForwardFold(queue, "", func(item string, accumulator string) string {
+		return accumulator + item
+	})
+	expectedConcatString := ""
+	for _, item := range items {
+		expectedConcatString += item
+	}
+
+	if concatString != expectedConcatString {
+		t.Errorf("result (%v) does not match expected result (%v)", concatString, expectedConcatString)
+	}
+}
+
+func TestReverseFold(t *testing.T) {
+	queue := arraystack.New[string]()
+	items := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	for _, item := range items {
+		queue.Add(item)
+	}
+
+	concatString := arraystack.ReverseFold(queue, "", func(item string, accumulator string) string {
+		return accumulator + item
+	})
+
+	slices.Reverse(items)
+	expectedConcatString := ""
+	for _, item := range items {
+		expectedConcatString += item
+	}
+
+	if concatString != expectedConcatString {
+		t.Errorf("result (%v) does not match expected result (%v)", concatString, expectedConcatString)
+	}
+}
